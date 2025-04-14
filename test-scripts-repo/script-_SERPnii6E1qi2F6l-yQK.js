cd9c250959c6936d9827859bf944b341f03d0a0c
Automated Script <automated@example.com>
2025-04-14 12:41:16 +0530

    Add test script for https://gazebosim.org/docs/all/tutorials/ (ID: _SERPnii6E1qi2F6l-yQK)

---
+++ test-scripts-repo/script-_SERPnii6E1qi2F6l-yQK.js
@@ -0,0 +1,5 @@
+// Test script for URL: https://gazebosim.org/docs/all/tutorials/
+// Generated on: 2025-04-14T07:11:16.642Z
+
+await page.goto('https://gazebosim.org/docs/all/tutorials/');
+  await page.getByText('Gazebo Tutorials# These').click();
