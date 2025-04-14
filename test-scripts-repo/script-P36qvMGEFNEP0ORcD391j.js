4bb6c9e210a436f80103b96257cd5d816a44c769
Kuldeep-Ifocus <kuldeep.m@ifocussystec.in>
2025-04-14 11:43:59 +0530

    Add test script for https://gazebosim.org/docs/all/tutorials/ (ID: P36qvMGEFNEP0ORcD391j)

---
+++ test-scripts-repo/script-P36qvMGEFNEP0ORcD391j.js
@@ -0,0 +1,5 @@
+// Test script for URL: https://gazebosim.org/docs/all/tutorials/
+// Generated on: 2025-04-14T06:13:59.566Z
+
+await page.goto('https://gazebosim.org/docs/all/tutorials/');
+  await page.getByRole('link', { name: 'Features', exact: true }).click();
