c8606dcdf7b3b196a69e95b9aa8efd77d34c5edd
Kuldeep-Ifocus <kuldeep.m@ifocussystec.in>
2025-04-14 11:17:20 +0530

    Add test script for https://gazebosim.org/docs/all/tutorials/ (ID: KkEE7n0VswPc_WhsIJ5oN)

---
+++ test-scripts-repo/script-KkEE7n0VswPc_WhsIJ5oN.js
@@ -0,0 +1,5 @@
+// Test script for URL: https://gazebosim.org/docs/all/tutorials/
+// Generated on: 2025-04-14T05:47:20.845Z
+
+await page.goto('https://gazebosim.org/docs/all/tutorials/');
+  await page.getByRole('heading', { name: 'GUI tutorials#' }).click();
