e1b58d98cf2a6f5b82084cbdf679a4861f0c4948
Automated Script <automated@example.com>
2025-04-14 12:14:22 +0530

    Add test script for https://gazebosim.org/docs/all/tutorials/ (ID: G19O6I5Jr_k5sEh43lmGU)

---
+++ test-scripts-repo/script-G19O6I5Jr_k5sEh43lmGU.js
@@ -0,0 +1,5 @@
+// Test script for URL: https://gazebosim.org/docs/all/tutorials/
+// Generated on: 2025-04-14T06:44:22.707Z
+
+await page.goto('https://gazebosim.org/docs/all/tutorials/');
+  await page.getByRole('link', { name: 'Showcase' }).click();
