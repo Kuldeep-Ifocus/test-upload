29e63683d6a60fa2f195e4a6df5b46c3f27197d6
Automated Script <automated@example.com>
2025-04-14 12:11:20 +0530

    Add test script for https://gazebosim.org/docs/all/tutorials/ (ID: rCsOfVPcOCfs2BqbxFHE_)

---
+++ test-scripts-repo/script-rCsOfVPcOCfs2BqbxFHE_.js
@@ -0,0 +1,5 @@
+// Test script for URL: https://gazebosim.org/docs/all/tutorials/
+// Generated on: 2025-04-14T06:41:20.815Z
+
+await page.goto('https://gazebosim.org/docs/all/tutorials/');
+  await page.getByRole('link', { name: 'SDF worlds', exact: true }).click();
